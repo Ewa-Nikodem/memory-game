b5259808fa0fea72e39df311766a36eacd53c34c
EwaNikodem <ewanikodem96@gmail.com>
2023-09-09 21:30:05 +0200

    finished game

--- src/script.js
+++ src/script.js
@@ -55,9 +55,10 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log(cardArray)
 
     const grid = document.querySelector('.grid');
-
+    const result = document.querySelector('#result');
     let cardsChosen = [];
     let cardsChosenIds = [];
+    let cardsWon = [];
 
     function createBoard() {
         for (let i = 0; i < cardArray.length; i++){
@@ -82,12 +83,37 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function checkForMatch() {
         const cards = document.querySelectorAll('img');
+        //const cardsName = document.querySelectorAll('name'); //i can store the names exactly the same as IDs as well
         const optionOneId = cardsChosenIds[0];
         const optionTwoId = cardsChosenIds[1]
         if (optionOneId === optionTwoId) {
             alert('You clicked the same card');
             cards[optionOneId].setAttribute('src', 'src/images/logo.webp');
+            cards[optionTwoId].setAttribute('src', 'src/images/logo.webp');
         }
+        else if(cardsChosen[0] === cardsChosen[1]) {
+            //alert('You have found a match');
+            cards[optionOneId].setAttribute('src', 'src/images/white.png');
+            cards[optionTwoId].setAttribute('src', 'src/images/white.png');
+            cards[optionOneId].removeEventListener('click', flipCard); //The removeEventListener() method removes an event handler from an element. We use it to prevent from clicking again on a pick
+            cards[optionTwoId].removeEventListener('click', flipCard);
+            cardsWon.push(cardsChosen);
+        }
+        else {
+            if (optionOneId != optionTwoId) {
+            cards[optionOneId].setAttribute('src', 'src/images/logo.webp');
+            cards[optionTwoId].setAttribute('src', 'src/images/logo.webp');
+        }
+        }
+        cardsChosen = []; //simple way to remove anything from previosuly populated array. So we can start over
+        cardsChosenIds = [];
+        result.textContent =  cardsWon.length; //display how many arrays are in the cardsWon
+        if (cardsWon.length === cardArray.length / 2) {
+            result.textContent = "You won"
+        }
+
+        console.log(cardsChosen);
+        console.log(cardsWon);
     }
 
     createBoard()
